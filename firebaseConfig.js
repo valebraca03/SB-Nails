f7b7fefab2523ada46a84fc177100f5172a4792c
valebraca03 <valebraca03@gmail.com>
2025-12-24 16:53:35 -0300

    nuevo

---
+++ firebaseConfig.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLbb62O-Z0Y5NKVXUxVkmPM6sYJaejrcI",
+  authDomain: "sofinail.firebaseapp.com",
+  projectId: "sofinail",
+  storageBucket: "sofinail.firebasestorage.app",
+  messagingSenderId: "520258822742",
+  appId: "1:520258822742:web:eeac6aeb11c4c3f4af91f7"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+  app,
+  auth,
+  db,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  doc,
+  getDoc,
+  setDoc
+};
